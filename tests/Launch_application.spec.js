498896e5544169358fd829bf76e2412bd709c57f
unknown <seemabhardwaj916@gmail.com>
2026-02-11 23:57:47 +0530

    Login done

--- tests/Launch_application.spec.js
+++ tests/Launch_application.spec.js
@@ -8,6 +8,11 @@ test ('Launch the application', async ({page})=>
     await page.goto("https://www.saucedemo.com/");
     await expect(page).toHaveTitle("Swag Labs");
     console.log(await page.title());
+    await page.locator('#user-name').fill("standard_user");
+    await page.locator('#password').fill("secret_sauce");
+    await page.locator('#login-button').click();
+    console.log("login done");
+    await page.pause();
 
 });
 
